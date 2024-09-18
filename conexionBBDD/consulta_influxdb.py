6eb82d21219dd2261eddf56f6dc0f5655454339a
Angela Fernandez <aangela.fdez02@gmail.com>
2024-09-18 18:16:47 +0200

    consultaBBDD_flux_vacia

--- conexionBBDD/consulta_influxdb.py
+++ conexionBBDD/consulta_influxdb.py
@@ -12,43 +12,33 @@ class InfluxDB:
     def __init__(self, bucketv2, orgv2, tokenv2, url='https://138.100.82.178:8086'):
         self.bucket = bucketv2
         self.org = orgv2
-        # Añadir verify_ssl=False para desactivar la verificación SSL
-        self.client = InfluxDBClient(url=url, token=tokenv2, org=self.org, verify_ssl=False)
+        # Aumentar timeout a 60 segundos
+        self.client = InfluxDBClient(url=url, token=tokenv2, org=self.org, verify_ssl=False, timeout=60_000)
         if self.bucket == 'Gait/autogen':
             self.measurement = ['Gait']
     
     def query_data(self):
-        # Consulta para valores de tipo float
-        query_float = f'''
+        # Consulta con rango limitado (últimos 30 días)
+        query = f'''
         from(bucket: "{self.bucket}")
-        |> range(start: 0)
+        |> range(start: -30d)
         |> filter(fn: (r) => r._measurement == "Gait")
-        |> filter(fn: (r) => r._value is float)
+        |> map(fn: (r) => ({{r with _value: float(v: r._value)}}))
         |> keep(columns: ["_time", "_value"])
-        '''
-        
-        # Consulta para valores de tipo int
-        query_int = f'''
-        from(bucket: "{self.bucket}")
-        |> range(start: 0)
-        |> filter(fn: (r) => r._measurement == "Gait")
-        |> filter(fn: (r) => r._value is int)
-        |> keep(columns: ["_time", "_value"])
-        '''
-        
-        result_float = self.client.query_api().query(org=self.org, query=query_float)
-        result_int = self.client.query_api().query(org=self.org, query=query_int)
-        
-        # Procesar los resultados de ambos en un dataframe
-        times = []
-        for table in result_float + result_int:
+    '''
+
+        # Realizar la consulta
+        result = self.client.query_api().query(org=self.org, query=query)
+
+        # Procesar los resultados
+        data = []
+        for table in result:
             for record in table.records:
-                times.append([record.get_time(), record.get_value()])
-        
-        df = pd.DataFrame(times, columns=["Time", "Value"])
-        
-        # Convertir todos los valores a float para evitar la colisión de tipos
-        df["Value"] = df["Value"].astype(float)
+                data.append([record.get_time(), record.get_value()])
+
+        # Crear DataFrame
+        df = pd.DataFrame(data, columns=["Time", "Value"])
+
         return df
 
 
@@ -61,6 +51,6 @@ url = "https://138.100.82.178:8086"
 # Inicializar el cliente de InfluxDB
 influx = InfluxDB(bucket, org, token, url)
 
-# Realizar la consulta y mostrar los tiempos
+# Realizar la consulta y mostrar los resultados
 df = influx.query_data()
 print(df)
