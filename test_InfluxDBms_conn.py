f71f43a00e2d55ae3854c49b65e3d6e3db2aef69
Angela Fernandez <aangela.fdez02@gmail.com>
2024-11-18 18:01:44 +0100

    actualizacion 18/11

--- test_InfluxDBms_conn.py
+++ test_InfluxDBms_conn.py
@@ -1,24 +1,97 @@
-"""
+# """
 
-Module: Test_DB_Connenction to InflixDB for Gait
+# Module: Test_DB_Connenction to InflixDB for Gait
 
-"""
+# """
+# import yaml,os, sys, pdb
+# import InfluxDBms
+# from pathlib import Path, PureWindowsPath
+
+# def main():
+#     """
+#     Main module testing two queries to the DB
+
+#     It does not receive parameters except those in the command line
+    
+#     It does not return specific variables, except those printed out.
+
+#     """
+#     # Leer la configuración desde el archivo YAML
+#     bp = BatchProcess()
+#     with open(Path(bp.get_cnf()), 'r') as file:
+#         config = yaml.safe_load(file)
+
+#     # Obtener los parámetros de conexión
+#     bucket = config['influxdb']['bucket']
+#     org = config['influxdb']['org']
+#     token = config['influxdb']['token']
+#     url = config['influxdb']['url']
+#     # Inicializar el cliente
+#     iDB = InfluxDB(bucket,org,token,url)
+
+#     # Realizar la consulta con las fechas de:  desde/hasta
+#     f_ini = "2024-11-02T18:08:45Z"
+#     f_end = "2024-11-02T18:50:00Z"
+#     (f_ini, f_end) = iDB.get_default_dates()
+
+#     # Consulta de influx
+#     df = iDB.query_data(f_ini,f_end)
+#     print(df)
+
+#     # # Consulta con agregación
+#     # window_size = "1m"  
+#     # df_agg = iDB.query_with_aggregate_window(f_ini, f_end, window_size)
+#     # print(df_agg)
+
+# if __name__ == "__main__":
+#     main()
+
+from pathlib import Path
+from InfluxDBms import *
+import argparse
+from datetime import *
 import yaml,os, sys, pdb
-import InfluxDBms
 from pathlib import Path, PureWindowsPath
+
+
+def get_default_dates():
+        """
+        Obtiene las fechas por defecto para la búsqueda.
+        """
+        today = datetime.now()
+        yesterday = today - timedelta(days=1)
+        default_from = yesterday.replace(hour=0, minute=0, second=0, microsecond=0)
+        default_until = yesterday.replace(hour=23, minute=59, second=59, microsecond=0)
+        return (default_from.strftime('%Y-%m-%d %H:%M:%S'), \
+                default_until.strftime('%Y-%m-%d %H:%M:%S'))
+
+
+def parse_args():
+    """
+    Configura y parsea los argumentos de entrada.
+    """
+    default_from, default_until = get_default_dates()
+    parser = argparse.ArgumentParser(description='Ejecución de procesos en batch.')
+    parser.add_argument('-f', '--from_time', type=str, default=default_from,
+                        help='Fecha de inicio de búsqueda (formato: YYYY-MM-DD HH:MM:SS)')
+    parser.add_argument('-u', '--until', type=str, default=default_until,
+                        help='Fecha de finalización de búsqueda (formato: YYYY-MM-DD HH:MM:SS)')
+    parser.add_argument('-p', '--path', type=str, default='InfluxDBms\config_db.yaml',  # Proporciona un valor por defecto
+                        help='Path al archivo de configuración config.yaml')
+    parser.add_argument('-v', '--verbose', nargs='?', action=VAction, dest='verbose',
+                        help='Nivel de verbosidad', default=0)
+    return parser.parse_args()
 
 def main():
     """
-    Main module testing two queries to the DB
-
-    It does not receive parameters except those in the command line
-    
-    It does not return specific variables, except those printed out.
-
+    Modulo principal que prueba dos consultas en la DB.
     """
+    args = parse_args()
+    bp = BatchProcess(args)  # Pasa los argumentos al BatchProcess
+
     # Leer la configuración desde el archivo YAML
-    bp = BatchProcess()
-    with open(Path(bp.get_cnf()), 'r') as file:
+    config_path = bp.get_cnf()
+    with open(Path(config_path), 'r') as file:
         config = yaml.safe_load(file)
 
     # Obtener los parámetros de conexión
@@ -26,22 +99,23 @@ def main():
     org = config['influxdb']['org']
     token = config['influxdb']['token']
     url = config['influxdb']['url']
-    # Inicializar el cliente
-    iDB = InfluxDB(bucket,org,token,url)
 
-    # Realizar la consulta con las fechas de:  desde/hasta
-    # from_date = "2024-10-01T00:00:00Z"
-    # to_date = "2024-10-21T00:00:00Z"
-    (f_ini, f_end) = iDB.get_default_dates()
+    # Inicializar el cliente
+    iDB = cInfluxDB(bucket, org, token, url)
+
+    # Realizar la consulta con las fechas de: desde/hasta
+    # f_ini = args.from_time
+    # f_end = args.until
+    f_ini = "2024-11-02T18:08:45Z"
+    f_end = "2024-11-02T18:50:00Z"
+    # (f_ini, f_end) = get_default_dates()
 
     # Consulta de influx
-    df = iDB.query_data(f_ini,f_end)
+    df = iDB.query_data(f_ini, f_end)
     print(df)
 
-    # Consulta con agregación
-    window_size = "10m"  
-    df_agg = iDB.query_with_aggregate_window(f_ini, f_end, window_size)
-    print(df_agg)
+    # Ejecución del proceso principal
+    bp.run()
 
 if __name__ == "__main__":
     main()
