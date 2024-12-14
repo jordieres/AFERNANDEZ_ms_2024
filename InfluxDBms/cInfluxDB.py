c4d48d35c71fe1d0bf9c3e1ad615b96f76134b29
Angela Fernandez <aangela.fdez02@gmail.com>
2024-12-14 19:39:22 +0100

    actualizacion 14/12

--- InfluxDBms/cInfluxDB.py
+++ InfluxDBms/cInfluxDB.py
@@ -1,3 +1,172 @@
+# import pandas as pd
+# from influxdb_client import InfluxDBClient
+# from datetime import datetime
+# import urllib3
+# import yaml
+
+# # Desactiva las advertencias de SSL
+# urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# class cInfluxDB:
+#     def __init__(self, config_path: str, timeout: int = 500_000):
+#         """
+#         Initializes the connection to InfluxDB using a YAML configuration file.
+
+#         :param config_path: Path to the YAML configuration file.
+#         :type config_path: str
+#         :param timeout: Connection timeout in milliseconds.
+#         :type timeout: int
+
+#         """
+#         # Carga la configuración desde el archivo YAML
+#         with open(config_path, 'r') as file:
+#             config = yaml.safe_load(file)
+
+#         # Extrae los valores necesarios
+#         self.bucket = config['influxdb']['bucket']
+#         self.org = config['influxdb']['org']
+#         self.token = config['influxdb']['token']
+#         self.url = config['influxdb']['url']
+
+#         # Inicializa el cliente de InfluxDB
+#         self.client = InfluxDBClient(url=self.url, token=self.token, org=self.org, verify_ssl=False, timeout=timeout)
+#         self.measurement = self.bucket.split("/")[0] if '/' in self.bucket else self.bucket
+
+    
+
+#     def query_data(self, from_date: datetime, to_date: datetime) -> pd.DataFrame:
+        
+#         """
+#         Query data in InfluxDB, pivoting the results to get the metrics in columns.
+
+#         :param from_date: Start date (ISO 8601 format: 'YYYYY'-MM-DDTHH:MM:SSZ).
+#         :type from_date: datetime
+#         :param to_date: End date (ISO 8601 format: 'YYYYY'-MM-DDTHH:MM:SSZ).
+#         :type to_date: datetime
+#         :return: DataFrame with the metrics pivoted on columns.
+#         rtype pd.DataFrame
+#         """
+
+#         from_date_str = from_date.strftime('%Y-%m-%dT%H:%M:%SZ')  # UTC con 'Z'
+#         to_date_str = to_date.strftime('%Y-%m-%dT%H:%M:%SZ')
+
+
+#         metrics = ['Ax', 'Ay', 'Az', 'Gx', 'Gy', 'Gz', 'Mx', 'My', 'Mz', 'S0', 'S1', 'S2']
+#         metrics_str = ' or '.join([f'r._field == "{metric}"' for metric in metrics])
+#         columns_str = ', '.join([f'"{metric}"' for metric in metrics])
+
+#         # from(bucket: "{self.bucket}")
+#         # |> filter(fn: (r) => r._measurement == "{self.measurement}")
+#         # |> filter(fn: (r) => {metrics_str})
+#         query = f'''
+#         from(bucket: "Gait/autogen") 
+#         |> range(start: time(v: "{from_date_str}"), stop: time(v: "{to_date_str}"))
+#         |> filter(fn: (r) => r._measurement == "Gait")
+#         |> filter(fn: (r) => r._field == "Ax" or r._field == "Ay" or r._field == "Az" or r._field == "Gx" or r._field == "Gy" or r._field == "Gz" or r._field == "Mx" or r._field == "My" or r._field == "Mz" or r._field == "S0" or r._field == "S1" or r._field == "S2")
+#         |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")
+#         |> keep(columns: ["_time", {columns_str}])
+#         '''
+#         print(f"Query generated: {query}")
+
+
+#         try:
+#             result = self.client.query_api().query(org=self.org, query=query)
+#         except Exception as e:
+#             print(f"Error in the query: {str(e)}")
+#             raise
+
+#         # Process the results in a aFrame
+#         data = []
+#         for table in result:
+#             for record in table.records:
+#                 row = {
+#                     "Time": record.get_time(),
+#                     **{record.get_field(): record.get_value() for field in metrics if field == record.get_field()}
+#                 }
+#                 data.append(row)
+
+#         return pd.DataFrame(data)
+
+
+#     def query_with_aggregate_window(self, from_date: datetime, to_date: datetime, window_size: str = "1d", qtok: str = None , pie: str = None) -> pd.DataFrame:
+#         """
+#         Query data in InfluxDB, pivoting the results to get the metrics in columns.
+#         Handles cases where there are no data points by using `aggregateWindow`.
+
+#         :param from_date: Start date (ISO 8601 format: 'YYYY-MM-DDTHH:MM:SSZ').
+#         :type from_date: datetime
+#         :param to_date: End date (ISO 8601 format: 'YYYY-MM-DDTHH:MM:SSZ').
+#         :type to_date: datetime
+#         :param window_size: Aggregation window size (e.g., '1s', '1d').
+#         :type window_size: str
+#         :return: DataFrame with the metrics pivoted on columns.
+#         :rtype: pd.DataFrame
+#         """
+#         from_date_str = from_date.strftime('%Y-%m-%dT%H:%M:%SZ')  # UTC con 'Z'
+#         to_date_str = to_date.strftime('%Y-%m-%dT%H:%M:%SZ')
+
+#         if not qtok or not pie:
+#             raise ValueError("Los argumentos 'qtok' y 'pie' son obligatorios para esta consulta.")
+
+#         metrics = ['Ax', 'Ay', 'Az', 'Gx', 'Gy', 'Gz', 'Mx', 'My', 'Mz', 'S0', 'S1', 'S2']
+#         metrics_str = ' or '.join([f'r._field == "{metric}"' for metric in metrics])
+#         columns_str = ', '.join([f'"{metric}"' for metric in metrics])
+
+#         # query = f'''
+#         # from(bucket: "{self.bucket}")
+#         #     |> range(start: time(v: "{from_date_str}"), stop: time(v: "{to_date_str}"))
+#         #     |> filter(fn: (r) => r._measurement == "{self.measurement}")
+#         #     |> filter(fn: (r) => {metrics_str})
+#         #     |> filter(fn: (r) => r["CodeID"] == "{qtok}" and r["type"] == "SCKS" and r["Foot"] == "{pie}")
+#         #     |> group(columns: ["_field"])
+#         #     |> aggregateWindow(every: {window_size}, fn: last, createEmpty: true)
+#         #     |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")
+#         #     |> keep(columns: ["_time", {columns_str}])
+#         # '''
+#         query= f'''
+#         from(bucket: "Gait/autogen")
+#             |> range(start: 2024-11-02T15:08:45Z, stop: 2024-11-02T20:50:00Z)
+#             |> filter(fn: (r) => r._measurement == "Gait")
+#             |> filter(fn: (r) => r._field == "Ax" or r._field == "Ay" or r._field == "Az" or r._field == "Gx" or r._field == "Gy" or r._field == "Gz" or r._field == "Mx" or r._field == "My" or r._field == "Mz" or r._field == "S0" or r._field == "S1" or r._field == "S2")
+#             |> filter(fn: (r) => r["CodeID"] == "JOM20241031-104" and r["type"] == "SCKS" and r["Foot"] == "Left")
+#             |> group(columns: ["_field"])
+#             |> aggregateWindow(every: 20ms, fn: last, createEmpty: true)
+#             |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")
+#             |> keep(columns: ["_time", "Ax", "Ay", "Az", "Gx", "Gy", "Gz", "Mx", "My", "Mz", "S0", "S1", "S2"])
+#         '''
+
+
+        
+#         print(f"Query generated with aggregate window: {query}")
+
+#         try:
+#             result = self.client.query_api().query(org=self.org, query=query)
+#         except Exception as e:
+#             print(f"Error in the query: {str(e)}")
+#             raise
+
+#         # Process the results into a DataFrame
+#         data = []
+#         for table in result:
+#             for record in table.records:
+#                 row = {"Time": record.get_time()}
+#                 for field in metrics:
+#                     if field == record.get_field():
+#                         row[field] = record.get_value()
+#                 data.append(row)
+
+#         # Convert the data to a DataFrame
+#         df = pd.DataFrame(data)
+
+#         # Ensure all required columns are present
+#         for col in ["Time"] + metrics:
+#             if col not in df:
+#                 df[col] = None  # Fill missing columns with None
+
+#         return df
+
+
+
 import pandas as pd
 from influxdb_client import InfluxDBClient
 from datetime import datetime
@@ -29,12 +198,13 @@ class cInfluxDB:
         self.url = config['influxdb']['url']
 
         # Inicializa el cliente de InfluxDB
-        self.client = InfluxDBClient(url=self.url, token=self.token, org=self.org, verify_ssl=False, timeout=timeout)
-        self.measurement = self.bucket.split("/")[0] if '/' in self.bucket else self.bucket
-
+        self.client = InfluxDBClient(url=self.url, token=self.token, org=self.org, \
+                                     verify_ssl=False, timeout=timeout)
+        self.measurement = self.bucket.split("/")[0] if '/' in self.bucket else \
+                           self.bucket
     
 
-    def query_data(self, from_date: datetime, to_date: datetime) -> pd.DataFrame:
+    def query_data(self, from_date: datetime, to_date: datetime, qtok: str, pie: str) -> pd.DataFrame:
         
         """
         Query data in InfluxDB, pivoting the results to get the metrics in columns.
@@ -55,19 +225,15 @@ class cInfluxDB:
         metrics_str = ' or '.join([f'r._field == "{metric}"' for metric in metrics])
         columns_str = ', '.join([f'"{metric}"' for metric in metrics])
 
-        # from(bucket: "{self.bucket}")
-        # |> filter(fn: (r) => r._measurement == "{self.measurement}")
-        # |> filter(fn: (r) => {metrics_str})
         query = f'''
-        from(bucket: "Gait/autogen") 
+        from(bucket: "{self.bucket}")
         |> range(start: time(v: "{from_date_str}"), stop: time(v: "{to_date_str}"))
-        |> filter(fn: (r) => r._measurement == "Gait")
-        |> filter(fn: (r) => r._field == "Ax" or r._field == "Ay" or r._field == "Az" or r._field == "Gx" or r._field == "Gy" or r._field == "Gz" or r._field == "Mx" or r._field == "My" or r._field == "Mz" or r._field == "S0" or r._field == "S1" or r._field == "S2")
+        |> filter(fn: (r) => r._measurement == "{self.measurement}")
+        |> filter(fn: (r) => {metrics_str})
+        |> filter(fn: (r) => r["CodeID"] == "{qtok}" and r["type"] == "SCKS" and r["Foot"] == "{pie}")
         |> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")
         |> keep(columns: ["_time", {columns_str}])
         '''
-        print(f"Query generated: {query}")
-
 
         try:
             result = self.client.query_api().query(org=self.org, query=query)
@@ -79,16 +245,14 @@ class cInfluxDB:
         data = []
         for table in result:
             for record in table.records:
-                row = {
-                    "Time": record.get_time(),
-                    **{record.get_field(): record.get_value() for field in metrics if field == record.get_field()}
-                }
-                data.append(row)
+                data.append(record.values)
 
-        return pd.DataFrame(data)
+        return(pd.DataFrame(data).drop(['result','table'],axis=1))
 
 
-    def query_with_aggregate_window(self, from_date: datetime, to_date: datetime, window_size: str = "1d", qtok: str = None , pie: str = None) -> pd.DataFrame:
+    def query_with_aggregate_window(self, from_date: datetime, to_date: datetime, \
+                                    window_size: str = "1d", qtok: str = None , \
+                                    pie: str = None) -> pd.DataFrame:
         """
         Query data in InfluxDB, pivoting the results to get the metrics in columns.
         Handles cases where there are no data points by using `aggregateWindow`.
@@ -106,7 +270,8 @@ class cInfluxDB:
         to_date_str = to_date.strftime('%Y-%m-%dT%H:%M:%SZ')
 
         if not qtok or not pie:
-            raise ValueError("Los argumentos 'qtok' y 'pie' son obligatorios para esta consulta.")
+            raise ValueError("Los argumentos 'qtok' y 'pie' son obligatorios " +\
+                             "para esta consulta.")
 
         metrics = ['Ax', 'Ay', 'Az', 'Gx', 'Gy', 'Gz', 'Mx', 'My', 'Mz', 'S0', 'S1', 'S2']
         metrics_str = ' or '.join([f'r._field == "{metric}"' for metric in metrics])
@@ -135,9 +300,7 @@ class cInfluxDB:
             |> keep(columns: ["_time", "Ax", "Ay", "Az", "Gx", "Gy", "Gz", "Mx", "My", "Mz", "S0", "S1", "S2"])
         '''
 
-
-        
-        print(f"Query generated with aggregate window: {query}")
+        # print(f"Query generated with aggregate window: {query}")
 
         try:
             result = self.client.query_api().query(org=self.org, query=query)
--- test_InfluxDBms_conn.py
+++ test_InfluxDBms_conn.py
@@ -1,3 +1,91 @@
+# """
+
+# Module: Test_DB_Connenction to InflixDB for Gait
+
+# """
+
+# import argparse
+# from datetime import datetime, timedelta
+# from dateutil.parser import parse as parse_date
+
+
+# from InfluxDBms.cInfluxDB import cInfluxDB
+# from InfluxDBms.fecha_verbose import BatchProcess, VAction
+
+# # Función para convertir cadenas a objetos datetime
+# def parse_datetime(value):
+#     try:
+#         return parse_date(value)  # Convierte cadenas ISO 8601 a objetos datetime
+#     except ValueError as e:
+#         raise argparse.ArgumentTypeError(f"Fecha inválida: {value}. Error: {e}")
+
+# # Fechas predeterminadas
+# def get_default_dates():
+
+#     """
+#     Gets the default dates for the search.
+#     """
+#     now = datetime.utcnow()
+#     return now.isoformat() + "Z", (now + timedelta(minutes=30)).isoformat() + "Z"
+
+# # Argumentos del programa
+# def parse_args():
+
+#     """
+#     Configures and parses input arguments.
+#     """
+#     default_from, default_until = get_default_dates()
+#     parser = argparse.ArgumentParser(description='Ejecución de procesos en batch.')
+#     parser.add_argument('-f', '--from_time', type=parse_datetime, default=default_from, help='Fecha inicio (ISO 8601)')
+#     parser.add_argument('-u', '--until', type=parse_datetime, default=default_until, help='Fecha fin (ISO 8601)')
+#     parser.add_argument('-q', '--qtok', type=str, required=True, help='CodeID')
+#     parser.add_argument('-pi', '--pie', type=str, required=True,choices=['Left', 'Right'], help='Elección de Pie Izquierdo o Derecho')
+#     parser.add_argument('-p', '--path', type=str, default='InfluxDBms/config_db.yaml', help='Path al archivo de configuración')
+#     parser.add_argument('-v', '--verbose', action='count', default=0, help='Nivel de verbosidad')
+
+#     return parser.parse_args()
+
+# # Función principal
+# def main():
+
+#     """
+#     Main module that executes an InfluxDB query to obtain a Dataframe with the requested data.    
+#     """
+#     args = parse_args()
+
+#     # Fechas de entrada (objetos datetime)
+#     from_time = args.from_time
+#     until_time = args.until
+#     qtok = args.qtok
+#     pie = args.pie
+
+
+#     try:
+#         iDB = cInfluxDB(config_path=args.path)
+#     except FileNotFoundError as e:
+#         print(f"Error: {e}")
+#         return
+#     except Exception as e:
+#         print(f"Error al inicializar cInfluxDB: {e}")
+#         return
+
+
+#     # Realizar consulta
+#     try:
+#         df = iDB.query_with_aggregate_window(from_time, until_time, window_size="1d", qtok= qtok , pie=pie )
+#         print("Resulados de la consulta:")
+#         print(df)
+#     except Exception as e:
+#         print(f"Error al consultar datos: {e}")
+#         return
+
+#     # Ejecutar el proceso batch
+#     bp = BatchProcess(args)
+#     bp.run()
+
+# if __name__ == "__main__":
+#     main()
+
 """
 
 Module: Test_DB_Connenction to InflixDB for Gait
@@ -5,7 +93,7 @@ Module: Test_DB_Connenction to InflixDB for Gait
 """
 
 import argparse
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from dateutil.parser import parse as parse_date
 
 
@@ -25,12 +113,11 @@ def get_default_dates():
     """
     Gets the default dates for the search.
     """
-    now = datetime.utcnow()
+    now = datetime.now(timezone.utc)
     return now.isoformat() + "Z", (now + timedelta(minutes=30)).isoformat() + "Z"
 
 # Argumentos del programa
 def parse_args():
-
     """
     Configures and parses input arguments.
     """
@@ -39,7 +126,7 @@ def parse_args():
     parser.add_argument('-f', '--from_time', type=parse_datetime, default=default_from, help='Fecha inicio (ISO 8601)')
     parser.add_argument('-u', '--until', type=parse_datetime, default=default_until, help='Fecha fin (ISO 8601)')
     parser.add_argument('-q', '--qtok', type=str, required=True, help='CodeID')
-    parser.add_argument('-pi', '--pie', type=str, required=True,choices=['Left', 'Right'], help='Elección de Pie Izquierdo o Derecho')
+    parser.add_argument('-l', '--leg', type=str, required=True,choices=['Left', 'Right'], help='Elección de Pie Izquierdo o Derecho')
     parser.add_argument('-p', '--path', type=str, default='InfluxDBms/config_db.yaml', help='Path al archivo de configuración')
     parser.add_argument('-v', '--verbose', action='count', default=0, help='Nivel de verbosidad')
 
@@ -57,7 +144,7 @@ def main():
     from_time = args.from_time
     until_time = args.until
     qtok = args.qtok
-    pie = args.pie
+    pie = args.leg
 
 
     try:
@@ -73,8 +160,10 @@ def main():
     # Realizar consulta
     try:
         df = iDB.query_with_aggregate_window(from_time, until_time, window_size="1d", qtok= qtok , pie=pie )
+        #df = iDB.query_data(from_time, until_time, qtok= qtok , pie=pie )
         print("Resulados de la consulta:")
         print(df)
+        print(df.shape)
     except Exception as e:
         print(f"Error al consultar datos: {e}")
         return
@@ -85,3 +174,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
