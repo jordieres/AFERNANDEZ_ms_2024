3c830138c7db33f448d974d36ddd4db7810f8607
jordieres <j.ordieres@upm.es>
2025-09-14 10:35:03 +0200

    Fixation for .readthedocs.yaml

--- docs/conf.py
+++ docs/conf.py
@@ -34,4 +34,5 @@ autodoc_typehints = "description"
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 html_static_path = ['_static']
